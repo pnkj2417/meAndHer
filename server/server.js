ff8bd342e2558800a47b428832f03ff657975f55
pankaj <pankajchauhan206@gmail.com>
2018-11-04 18:37:23 +0530

    first msg

---
+++ server/server.js
@@ -0,0 +1,38 @@
+var express             =require('express');
+var app                  =express();
+const socketIO          =require('socket.io');
+const http              =require('http');
+
+const path=require('path');
+const publicPath=path.join(__dirname,"../public");
+const port= process.env.PORT || 3000;
+app.use(express.static(publicPath));
+
+var server=http.createServer(app);
+app.get('/',function(req,res){
+    app.render('index');
+});
+
+var io=socketIO(server);
+io.on('connection',(socket)=>{
+    //code to run as new user connected..
+    console.log("new user connected");
+    socket.on('disconnect',function(){
+        console.log("user was disconnected..");
+    });
+    
+    socket.on('createMsg',function(msg){
+        console.log(msg.text);
+        io.emit('newMsg',{
+            from:"pankaj",
+            text:"a message",
+           // createdAt:new date.getTime()
+        });
+    });
+});
+
+
+
+server.listen(port,()=>{
+    console.log("server started");
+});
